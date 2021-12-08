396024cb0665c072c96412416308e4112008211a
Sebastian Laursen <sebastian.theodor.laursen@gmail.com>
2021-12-08 23:16:11 +0100

    Firts commit. Blog + Rentalbrowser + RentalCreator start

---
+++ SurfsUpBlazor/Client/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SurfsUpBlazor.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("SurfsUpBlazor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("SurfsUpBlazor.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+
+await builder.Build().RunAsync();
---
+++ SurfsUpBlazor/Shared/Models/RentalPost.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurfsUpBlazor.Shared.Models
+{
+    public class RentalPost
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Title is too long.")]
+        public string Title { get; set; } = "Title";
+
+        [Required]
+        [RegularExpression(@".*\S+.*$", ErrorMessage = "Field Cannot be Blank Or Whitespace")]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be a positive number")]
+        public int Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be a positive number")]
+        public int Deposit { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Description is too long.")]
+        public string Description { get; set; } = "Description";
+
+        [Required]
+        public DateTime AvailableDateFrom { get; set; } = DateTime.Now.Date;
+
+        [Required]
+        public DateTime AvailableDateTo { get; set; } = DateTime.Now.Date.AddDays(1);     
+        
+        public DateTime PostedDate { get; private set; } = DateTime.Now.Date;
+
+        public override string ToString()
+        {
+            return $"{Title}, {Price}DKK, PostDate {PostedDate:dd/MM/yyyy}";
+        }
+    }
+}
